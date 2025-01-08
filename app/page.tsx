fe04bdb048f507495cc545a0e84e06ed34c0cfca
linh.cntt097@gmail.com <linh.cntt097@gmail.com>
2025-01-08 22:29:19 +0700

    Init project, base theme and font

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import ThemeToggles from '@/components/ThemeToggle'
+
+export default function Home() {
+  return (
+    <div className='w-full container p-4 mx-auto'>
+      <div className='py-20 flex flex-col items-center justify-center text-gray-800 dark:text-gray-100'>
+        <h1 className='text-5xl text-center font-bold font-vibes'>
+          Next Themes + Tailwind +{' '}
+          <span className='text-primary-foreground bg-primary py-2 px-4 rounded'>
+            Multi
+          </span>{' '}
+          Themes
+        </h1>
+        <p className='italic text-2xl'>with app-dir</p>
+        <ThemeToggles />
+      </div>
+    </div>
+  )
+}
